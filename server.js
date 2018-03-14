4b444d5e1c9e684c601fa8c106415b648bcc8788
Practicante CI APT <practicanteciapt@prolamsa.com>
2018-03-14 15:17:45 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//SERVER JS
+
+const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
+const bodyparser = require('body-parser');
+
+const app = express();
+
+app.listen(port)
